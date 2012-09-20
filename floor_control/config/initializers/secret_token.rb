7ec3160ecab50a464cfd10123a25f1227bf6e129
juan broz <f870421@gmail.com>
2012-09-20 14:09:48 -0500

    created the basic rails app

---
+++ floor_control/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FloorControl::Application.config.secret_token = '6922d82ab9c9b095af05b8c75dd79800c5a7e15a234af25d973b050fed07154df08c612cdfebf7954cf2aed920183e683b9a21b4f2af99b2008f26d6921e9567'
